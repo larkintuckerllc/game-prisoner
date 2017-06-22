8c617af881ea09f9bd3d1d0ad2311d84140844ed
John Tucker <john@larkintuckerllc.com>
2017-06-22 19:02:08 -0400

    roughed in Firebase

--- src/strings.js
+++ src/strings.js
@@ -1,2 +1,12 @@
-// eslint-disable-next-line
 export const ACTION_PREFIX = 'app/';
+export const FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyD0xUucxtbfOc_0lp_w4nv69LIle2HTnNU',
+  authDomain: 'game-prisoner.firebaseapp.com',
+  databaseURL: 'https://game-prisoner.firebaseio.com',
+  projectId: 'game-prisoner',
+  storageBucket: 'game-prisoner.appspot.com',
+  messagingSenderId: '416710348817',
+};
+export const FIREBASE_EMAIL = 'player@game.prisoner';
+// TODO: EXPLORE HAVING TO PROVIDE
+export const FIREBASE_PASSWORD = 'GPonly00';
